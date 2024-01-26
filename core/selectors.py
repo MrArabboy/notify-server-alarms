4197f3fc3647136126612b5975bf028821c2c896
Arabboy <mr.arabboy@gmail.com>
2024-01-26 09:59:11 +0500

    send only Critical level logs

--- core/selectors.py
+++ core/selectors.py
@@ -11,9 +11,9 @@ def get_all_incoming_alarms():
     alarms = client.alarms(offset=0, limit=100)
     root = Root.from_dict(alarms)
     required_num_of_request = root.total // 100
-    items = root.items
+    items = [item for item in root.items if item.iAlarmLevel == "Critical"]
     for page_num in range(1, required_num_of_request + 1):
         alarms = client.alarms(offset=page_num * 100, limit=100)
-        root = Root.from_dict(alarms)
-        items.extend(root.items)
+        new_root = Root.from_dict(alarms)
+        items.extend([item for item in new_root.items if item.iAlarmLevel == "Critical"])
     return items
